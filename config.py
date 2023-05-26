2de76ee5e5de323c962e6ac80314329e21f79810
YASH <129831825+PRADHAN474@users.noreply.github.com>
2023-05-26 08:08:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ from data import THE_ALTS
 API_ID = int(getenv("API_ID", "25981592"))
 API_HASH = getenv("API_HASH", "709f3c9d34d83873d3c7e76cdd75b866")
 SESSION1 = getenv("SESSION")
-ALIVE_PIC = getenv("ALIVE_PIC", "https://te.legra.ph/file/07d39b85c6cea32f15259.jpg")
-OWNER_ID = int(getenv("OWNER_ID", "5518687442"))
+ALIVE_PIC = getenv("ALIVE_PIC", "https://graph.org/file/ae522353ddffb830dd83e.jpg")
+OWNER_ID = int(getenv("OWNER_ID", "5059737154"))
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
